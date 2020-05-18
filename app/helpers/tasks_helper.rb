2769fde7392414a2a2b2763eec659a423a36773c
blee1710 <s3707616@student.rmit.edu.au>
2020-05-18 17:55:50 +1000

    heroku fix

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,3 +1,5 @@
-def checked(area)
-    @tasks.skills.nil? ? false : @tasks.skills.match(area)
+module TasksHelper
+  def checked(area)
+      @tasks.skills.nil? ? false : @tasks.skills.match(area)
+  end
 end
